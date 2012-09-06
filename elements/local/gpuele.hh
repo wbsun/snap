576507c23e9cfb0042bc2333aaf461d8f686860d
Weibin Sun <wbsun@flux.utah.edu>
2012-09-06 17:30:03 -0600

    GPU element done

--- elements/local/gpuele.hh
+++ elements/local/gpuele.hh
@@ -7,24 +7,40 @@
 #include <click/pair.hh>
 #include <click/timer.hh>
 #include <click/task.hh>
+#include <click/deque.hh>
 #include <g4c.h> // GPU for Click header
 CLICK_DECLS
 
+/**
+ * This element doesn't consider thread-safe, though it uses multi-threading itself.
+ */
 class GPUEle : public Element
 {
 public:
-	typedef Vector<Packet*> PacketVector;
-	typedef Pair<PacketVector*, void*> PacketBatch;
+	typedef Deque<Packet*> PacketQueue;
+	typedef Pair<PacketQueue*, unsigned char*> PacketBatch;
 
 private:
-	int _count;
+	unsigned long _count;
+	unsigned long _dropped;
+	
 	size_t _pktbufsz;
 	unsigned int _batchsz;
 	unsigned int _ustimeout;
 	PacketBatch _curbatch;
-	Vector<PacketBatch> _batches;
+	PacketBatch _pullbatch;
+	Deque<PacketBatch> _todobatches;
+	Deque<PacketBatch> _donebatches;
 
-	PacketBatch init_batch();
+	Deque<PacketBatch> _batchpool;
+
+	Task *_task;
+
+	PacketBatch new_batch();
+	bool init_batch_pool();
+	void free_batch(PacketBatch pb);
+	PacketBatch alloc_batch();
+	void free_batch_pool();
 
 public:
 	static const size_t DEFAULT_PACKET_BUF_SZ = 4096; // Page size
