02a14bcc5ca6d1caa8def28fe103c152582ce251
Weibin Sun <wbsun@flux.utah.edu>
2012-11-06 11:55:47 -0700

    Simple GPURuntime to initialize and cleanup G4C lib.

---
+++ elements/local/gpuruntime.cc
@@ -0,0 +1,59 @@
+#include <click/config.h>
+#include "gpuruntime.hh"
+#include <click/error.hh>
+#include <click/glue.hh>
+#include <click/standard/alignmentinfo.hh>
+#include <click/hvputils.hh>
+#include <click/confparse.hh>
+#include <g4c.h>
+CLICK_DECLS
+
+GPURuntime::GPURuntime() {
+	_hostmem_sz = G4C_DEFAULT_MEM_SIZE;
+	_devmem_sz = G4C_DEFAULT_MEM_SIZE;
+	_nr_streams = G4C_DEFAULT_NR_STREAMS;
+}
+
+GPURuntime::~GPURuntime() {}
+
+int
+GPURuntime::configure(Vector<String>&, ErrorHandler* errh)
+{
+	int ns = 0;
+	size_t hsz = 0, dsz = 0;
+	
+	if (cp_va_kparse(conf, this, errh,
+			 "STREAMS", cpkN, cpInteger, &ns,
+			 "HOSTMEMSZ", cpkN, cpSize, &hsz,
+			 "DEVMEMSZ", cpkN, cpSize, &dsz,
+			 cpEnd) < 0)
+		return -1;
+
+	if (ns)
+		_nr_streams = ns;
+	if (hsz)
+		_hostmem_sz = hsz;
+	if (dsz)
+		_devmem_sz = dsz;
+
+	if (g4c_init(_nr_streams, _hostmem_sz, _devmem_sz)) {
+		errh->error("GPURuntime G4C initialization failed!\n");
+		hvp_chatter("GPURuntime G4C initialization failed!\n");
+		return -1;
+	}
+	return 0;
+}
+
+void
+GPURuntime::cleanup(CleanupStage stage)
+{
+	if (stage == CLEANUP_CONFIGURED ||
+	    stage == CLEANUP_INITIALIZE_FAILED ||
+	    stage == CLEANUP_INITIALIZED ||
+	    stage == CLEANUP_ROUTER_INITIALIZED )
+		g4c_exit();
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(GPURuntime)
+ELEMENT_LIBS(-lg4c)
--- elements/local/gpuruntime.hh
+++ elements/local/gpuruntime.hh
@@ -1,13 +1,22 @@
 #ifndef CLICK_GPU_RUNTIME_HH
 #define CLICK_GPU_RUNTIME_HH
-
 #include <click/element.hh>
-
-#include <g4c.h>
 CLICK_DECLS
 
 class GPURuntime : public Element
 {
+public:
+	GPURuntime();
+	~GPURuntime();
+
+	const char *class_name() const	{ return "GPURuntime"; }
+	int configure_phase() const	{ return CONFIGURE_PHASE_INFO; }
+	int configure(Vector<String>&, ErrorHandler*);
+	void cleanup(CleanupStage stage);
+private:
+	size_t _hostmem_sz;
+	size_t _devmem_sz;
+	int _nr_streams;
 }
 
 CLICK_ENDDECLS
