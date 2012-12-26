1ab1c957528c7c704d3148f599fbbe867541bae9
Weibin Sun <wbsun@flux.utah.edu>
2012-12-26 01:26:46 -0700

    A powerful packet batch and its producers

--- include/click/pbatch.hh
+++ include/click/pbatch.hh
@@ -1,6 +1,5 @@
 #ifndef CLICK_PBATCH_HH
 #define CLICK_PBATCH_HH
-// I don't and don't want to know why putting this header at the beginning can avoid compilation errors.
 #include <click/ipaddress.hh>
 #include <click/glue.hh>
 #include <g4c.h>
@@ -12,6 +11,251 @@ class Packet;
 
 CLICK_DECLS
 
+class PBatch;
+
+#ifndef CLICK_PBATCH_NR_RANGES
+#define CLICK_PBATCH_NR_RANGES 8
+#endif
+
+struct PSliceRange {
+#define pslice_special_range(psr) ((psr).start < 0)
+#define PSLICE_FROM_ETH_HDR (-4096)
+#define PSLICE_FROM_IP_HDR  (-4097)
+#define PSLICE_FROM_IP_EXTENSION (-4098)
+#define PSLICE_FROM_UDP_HDR (-4099)
+#define PSLICE_FROM_TCP_HDR (-4100)
+#define PSLICE_FROM_IP_PAYLOAD (-4101)
+#define PSLICE_FROM_UDP_PAYLOAD (-4102)
+#define PSLICE_FROM_TCP_PAYLOAD (-4103)
+    int16_t start;
+    int16_t start_offset;
+
+#define pslice_special_length(psr) ((psr).len <= 0)
+#define pslice_real_length(psr) ((psr).len < 0? -1*(psr).len:(psr).len)
+#define PSLICE_LEN_UNKNOWN 0
+    int16_t len;
+    int16_t end;
+
+    int16_t slice_offset;
+
+    struct PSliceRange& operator=(const struct PSliceRange& v) {
+	this->start = v.start;
+	this->start_offset = v.start_offset;
+	this->len = v.len;
+	this->end = v.end;
+	this->slice_offset = v.offset;
+	return *this;
+    }
+};
+
+/*
+ * Usage:
+ *       Configuration phase: out of order
+ *             set_batch_size();  // optional, self calls
+ *             set_need_lens();   // optional, self or others call
+ *             req_slice_raneg(); // optional, multiple times by others
+ *             req_anno();        // optional, multiple times by others
+ *             req_priv_data();   // optional, multiple times by others
+ *
+ *       After other's configuration, in order by self
+ *             setup_anno();
+ *             setup_slice_ranges();
+ *             setup_mm();
+ *             or simply setup().
+ *
+ *             Then initialize PBatch pool if exists.
+ *
+ * For PBatch creation:
+ *       pb = new PBatch(self);  
+ *       pb->init();
+ *       alloc_batch_priv_data(pb);
+ *       assign_batch_mem(pb, ... );
+ *
+ * For PBatch recyling:
+ *       pb->recyle();
+ */
+class BatchProducer {
+public:
+    BatchProducer() {
+	this->init_slice_ranges();
+	this->init_anno();
+	this->init_mm();
+    }
+
+
+    //
+    // Packet slice settings:
+    //
+protected:    
+    int nr_slice_ranges_req;
+    struct PSliceRange req_slice_ranges[CLICK_PBATCH_NR_RANGES];
+    int nr_slice_ranges;
+    struct PSliceRange slice_ranges[CLICK_PBATCH_NR_RANGES];
+
+    int16_t slice_length; // = -1
+    int16_t slice_stride; // = -1
+
+    virtual void calculate_slice_args();
+
+public:
+    int get_nr_slice_ranges();
+    virtual void init_slice_ranges();
+    
+    // Return idx of allocated slice range in req_slice_ranges,
+    //  or -1 if full.
+    virtual int req_slice_range(PSliceRange &psr);
+
+    // Could be no solution, and whole packet as a result.
+    virtual void setup_slice_ranges() = 0;
+
+    // Return offset of request slice in slice buffers, or -1 if not exist.
+    virtual int16_t get_slice_offset(const PSliceRange &psr) = 0;
+
+    virtual int16_t get_slice_length();
+    virtual int16_t get_slice_stride();
+
+    
+    //
+    // Annotations:
+    //
+public:
+    uint8_t w_anno_start;
+    uint8_t w_anno_len;
+    uint8_t r_anno_start;
+    uint8_t r_anno_len;
+
+    uint8_t anno_start;
+    uint8_t anno_len;
+
+public:
+    enum {anno_write = 0x1, anno_read = 0x2};
+
+    virtual void init_anno();    
+    virtual int req_anno(uint8_t start, uint8_t len, uint8_t rw);
+    virtual void setup_anno();
+    virtual int8_t get_anno_offset(uint8_t start);
+
+    
+    //
+    // Memory management:
+    //
+public:
+    int batch_size;
+    size_t mem_size;
+    int lens_offset;
+    int slices_offset;
+    int annos_offset;
+    bool need_lens;
+    
+public:
+    void set_batch_size(int bsz) { batch_size = bsz; }
+    void set_need_lens() { need_lens = true; }
+    virtual int assign_batch_mem(PBatch *pb, void* hm,
+				 void *dm, size_t msz);
+    virtual void init_mm();
+    virtual void setup_mm();
+
+
+    //
+    // Private data for batch users
+    //
+public:
+    int nr_batch_users;
+    size_t batch_priv_len;
+
+public:
+    // Return offset of request data in private buffer.
+    virtual size_t req_priv_data(size_t len);
+    virtual void init_priv_data();
+
+    // Return negative value on error 
+    virtual int alloc_batch_priv_data(PBatch *pb);
+    
+public:
+    // Call after init_xxx, req_xxx, set_xxx:
+    virtual void setup() {
+	this->setup_anno();
+	this->setup_slice_ranges();
+	this->setup_mm();
+    }
+
+    // Return 0 for OK recycled
+    //        1 for not recycle-able
+    //        negative for errors
+    virtual int recycle_batch(PBatch *pb) = 0;
+
+    // Try to destroy batch:
+    //        if pb shared by others, just dec shared.
+    //        if not shared, try recycle.
+    //        if not recycle-able, destroy it.
+    virtual int kill_batch(PBatch *pb) = 0;
+
+    friend class PBatch;
+};
+
+class EthernetBatchProducer : public BatchProducer {
+public:
+    EthernetBatchProducer() : BatchProducer() {}
+    
+    static const int16_t eth_hdr;
+    static const int16_t ip4_hdr;
+    static const int16_t ip4_ext;
+    static const int16_t arp_hdr;
+    static const int16_t udp4_hdr;
+    static const int16_t tcp4_hdr;
+    static const int16_t eth_payload;
+    static const int16_t ip4_payload;
+    static const int16_t udp4_payload;
+    static const int16_t tcp4_payload;
+    static const int16_t ip6_hdr;
+    static const int16_t ip6_ext;
+    static const int16_t ip6_payload;
+    static const int16_t udp6_hdr;
+    static const int16_t tcp6_hdr;
+    static const int16_t udp6_payload;
+    static const int16_t tcp6_payload;
+
+    // To limit wasted bandwidth becasue of range merging.
+    //    factor: useless_len / (useless_len + effective_len)
+    static float merge_threshold;
+    
+    virtual void setup_slice_ranges();
+    virtual int16_t get_slice_offset(const PSliceRange &psr);
+
+    virtual int recycle_batch(PBatch *pb) = 0;
+    virtual int kill_batch(PBatch *pb) = 0;
+
+    firend class PBatch;
+};
+
+class PBatch {
+public:
+    BatchProducer *producer;
+    int npkts;
+    Packet **pptrs;
+
+    void *host_mem;
+    void *dev_mem;
+
+    int dev_stream;
+    void *hwork_ptr;
+    void *dwork_ptr;
+    int work_size;
+
+    int shared;
+
+    void *priv_data;
+
+public:
+    PBatch(BatchProducer *prod);
+    virtual ~PBatch();
+
+    void init();
+    void finit();
+};
+
+#if 0  // Temporarily keep this because of lagecy uses of PBatch.
+ 
 class PBatch {
 public:
     int capacity;
@@ -33,7 +277,7 @@ public:
      *  ~  For roundup to PAGE_SIZE        ~
      *  |----------------------------------|
      *  |                                  |
-     *  |  N * unsigned int: packet flags |
+     *  |  N * unsigned int: packet flags  |
      *  |                                  | 
      *  |----------------------------------|
      *  ~  For roundup to PAGE_SIZE        ~
@@ -149,6 +393,8 @@ public:
 
     inline void *get_user_priv(unsigned long offset) { return (void*)(g4c_ptr_add(user_priv, offset)); }
 };
+
+#endif
 
 CLICK_ENDDECLS
 #endif
--- lib/pbatch.cc
+++ lib/pbatch.cc
@@ -3,7 +3,413 @@
 #include <click/pbatch.hh>
 #include <g4c.h>
 
+#include <utility>
+#include <list>
+using namespace std;
+
 CLICK_DECLS
+
+int
+BatchProducer::get_nr_slice_ranges()
+{
+    return nr_slice_ranges;
+}
+
+void
+BatchProducer::init_slice_ranges()
+{
+    nr_slice_ranges_req = 0;
+    nr_slice_ranges = 0;
+    slice_length = -1;
+    slice_stride = -1;
+}
+
+void
+BatchProducer::calculate_slice_args()
+{
+    slice_length = 0;
+    slice_stride = 0;
+
+    for (int i=0; i<nr_slice_ranges; i++)
+	slice_length += pslice_real_length(slice_ranges[i]);
+
+    if (slice_length)
+	slice_stride = g4c_round_up(slice_length, G4C_MEM_ALIGN);
+}
+
+int16_t
+BatchProducer::get_slice_length()
+{
+    if (slice_length < 0)
+	this->calculate_slice_args();
+
+    return slice_length;
+}
+
+int16_t
+BatchProducer::get_slice_stride()
+{
+    if (slice_stride < 0)
+	this->calculate_slice_args();
+
+    return slice_stride;
+}
+
+int
+BatchProducer::req_slice_range(PSliceRange &psr)
+{
+    if (nr_slice_ranges_req >= CLICK_PBATCH_NR_RANGES)
+	return -1;
+    
+    req_slice_ranges[nr_slice_ranges_req] = psr;
+    return nr_slice_ranges_req++;
+}
+
+
+#define __min(a,b) ({						\
+	    typeof(a) _a = a;					\
+	    typeof(b) _b = b;					\
+	    _a < _b? _a:_b; })
+#define __max(a,b) ({						\
+	    typeof(a) _a = a;					\
+	    typeof(b) _b = b;					\
+	    _a > _b? _a:_b; })
+#define __merge_anno_range(sdst, ldst, snew, lnew) {		\
+	if ((ldst) == 0) {					\
+	    ldst = (lnew);					\
+	    sdst = (snew);					\
+	} else {						\
+	    typeof(sdst) ns = 0, ne = 0;			\
+	    ns = __min(sdst, snew);				\
+	    ne = __max((sdst)+(ldst), (snew)+(lnew));		\
+	    sdst = ns;						\
+	    ldst = ne - ns; }
+
+    
+void
+BatchProducer::init_anno()
+{
+    w_anno_start = 0;
+    w_anno_len = 0;
+    r_anno_start = 0;
+    r_anno_len = 0;
+    anno_start = 0;
+    anno_len = 0;
+}
+
+int
+BatchProducer::req_anno(uint8_t start, uint8_t len, uint8_t rw)
+{
+    if (rw & anno_write)	    
+	__merge_anno_range(w_anno_start, w_anno_len,
+			   start, len);
+    if (rw & anno_read)
+	__merge_range(r_anno_start, r_anno_len,
+		      start, len);
+    return 0;
+}
+
+void
+BatchProducer::setup_anno()
+{
+    anno_len = w_anno_len;
+    anno_start = w_anno_start;
+    if (r_anno_len != 0)
+	__merge_anno_range(anno_start, anno_len,
+			   r_anno_start, r_anno_len);
+}
+
+int8_t
+BatchProducer::get_anno_offset(uint8_t start)
+{
+    if (start >= anno_start)
+	return start - anno_start;
+    return -1;
+}
+
+int
+BatchProducer::assign_batch_mem(PBatch *pb, void *hm, void *dm, size_t msz)
+{
+    pb->host_mem = hm;
+    pb->dev_mem = dm;    
+}
+
+void
+BatchProducer::init_mm()
+{
+    mem_size = 0;
+    lens_offset = -1;
+    slices_offset = -1;
+    annos_offset = -1;
+    need_lens = false;
+    batch_size = CLICK_PBATCH_CAPACITY;
+}
+
+void
+BatchProducer::setup_mm()
+{
+    if (need_lens) {
+	mem_size += g4c_round_up(batch_size*sizeof(int16_t), G4C_PAGE_SIZE);
+	lens_offset = 0;
+    }
+    
+    if (this->get_slice_stride() && batch_size) {
+	slices_offset = mem_size;
+    }
+    mem_size += g4c_round_up(batch_size*this->get_slice_stride(), G4C_PAGE_SIZE);
+
+    if (anno_len != 0) {
+	annos_offset = mem_size;
+	mem_size += g4c_round_up(batch_size*anno_len, G4C_PAGE_SIZE);
+    }    
+}
+
+void
+BatchProducer::init_priv_data()
+{
+    nr_batch_users = 0;
+    batch_priv_len = 0;
+}
+
+size_t
+BatchProducer::req_priv_data(size_t len)
+{
+    nr_batch_users++;
+    size_t pos = batch_priv_len;
+    batch_priv_len += len;
+
+    return pos;
+}
+
+int
+BatchProducer::alloc_batch_priv_data(PBatch *pb)
+{
+    if (pb->priv_data)
+	free(pb->priv_data);
+
+    pb->priv_data = 0;
+    if (batch_priv_len) {
+	pb->priv_data = malloc(batch_priv_len);
+	if (!pb->priv_data) {
+	    click_chatter("PBatch out of mem for private data.\n");
+	    return -1;
+	}
+    }
+
+    return 0;
+}
+
+
+// EthernetBatchProducer
+
+const int16_t EthernetBatchProducer::eth_hdr      = 0;
+const int16_t EthernetBatchProducer::ip4_hdr      = 14;
+const int16_t EthernetBatchProducer::ip4_ext      = 34;
+const int16_t EthernetBatchProducer::arp_hdr      = 14;
+const int16_t EthernetBatchProducer::udp4_hdr     = 34;
+const int16_t EthernetBatchProducer::tcp4_hdr     = 34;
+const int16_t EthernetBatchProducer::eth_payload  = 14;
+const int16_t EthernetBatchProducer::ip4_payload  = 34;
+const int16_t EthernetBatchProducer::udp4_payload = 42;
+const int16_t EthernetBatchProducer::tcp4_payload = 54;
+const int16_t EthernetBatchProducer::ip6_hdr      = 14;
+const int16_t EthernetBatchProducer::ip6_ext      = 54;
+const int16_t EthernetBatchProducer::ip6_payload  = 54;
+const int16_t EthernetBatchProducer::udp6_hdr     = 54;
+const int16_t EthernetBatchProducer::tcp6_hdr     = 54;
+const int16_t EthernetBatchProducer::udp6_payload = 62;
+const int16_t EthernetBatchProducer::tcp6_payload = 74;
+
+float EthernetBatchProducer::merge_threshold = 0.2f;
+
+
+static bool
+__comp_slice_range(pair<int16_t, int16_t> a, pair<int16_t, int16_t> b)
+{
+    return a.first <= b.first;
+}
+
+void
+EthernetBatchProducer::setup_slice_ranges()
+{
+    // Rare case:
+    if (nr_slice_ranges_re1 == 0) {
+	nr_slice_ranges = 0;
+	return;
+    }
+    
+    // Special but may be common case:
+    if (nr_slice_ranges_req == 1)
+    {
+	nr_slice_ranges = 1;
+
+	slice_ranges[0].start = req_slice_ranges[i].start+
+	    req_slice_ranges[i].start_offset;
+	slice_ranges[0].start_offset = 0;
+	slice_ranges[0].len = pslice_real_length(req_slice_rangs[i]);
+	return;
+    }
+
+    
+    // Then convert ranges to pairs:
+    list<pair<int16_t, int16_t> > l1, l2, *pl1, *pl2, *tmp;
+
+    pl1 = &l1;
+    pl2 = &l2;
+    for (int i = 0; i<nr_slice_ranges_req; i++) {
+	int16_t ns = req_slice_ranges[i].start+
+	    req_slice_ranges[i].start_offset;
+	int16_t ne = ns+pslice_real_length(req_slice_ranges[i]);
+
+	pl1->push_back(make_pair(ns, ne));
+    }
+
+    // Sort so that go from the beginning:
+    pl1->sort(__comp_slice_range);
+
+    // First merge, which merges overlapped ranges only:
+    int16_t tlen = 0;
+    list<pair<int16_t, int16_t> >::ite = pl1->begin();
+    pair<int16_t, int16_t> prev = *ite;
+    while(ite++ != pl1->end())
+    {
+	pair<int16_t, int16_t> cur = *ite;
+
+	if (cur.first <= prev.second)
+	{
+	    pair<int16_t, int16_t> t;
+	    
+	    t.first = prev.first;
+	    t.second = __max(prev.second, cur.second);
+	    prev = t;
+	}
+	else
+	{
+	    pl2->push_back(prev);
+	    tlen += prev.second - prev.first;
+	    prev = cur;
+	}
+    }
+    pl2->push_back(prev);
+    tlen += prev.second - prev.first;
+
+    pl1->clear();
+    tmp = pl1;
+    pl1 = pl2;
+    pl2 = tmp;
+
+    // Then merge non-overlapped ranges, gaps between which are
+    //   larger than merge_threshold * tlen.
+    if (pl1->size() > 1)
+    {
+	bool merged;
+	do {
+	    merged = false;
+	    
+	    ite = pl1->begin();
+	    prev = *ite;
+	    while (ite++ != pl1->end()) {
+		pair<int16_t, int16_t> cur = *ite;
+
+		if ((float)(((float)(cur.first - prev.second))/((float)(tlen)))
+		    >= EthernetBatchProducer::merge_threshold) {
+		    tlen += cur.first-prev.second;
+
+		    prev.second = cur.second;
+		    merged = true;
+		} else {
+		    pl2->push_back(prev);
+		    prev = cur;
+		}		
+	    }
+	    pl2->push_back(prev);
+
+	    pl1->clear();
+	    tmp = pl1;
+	    pl1 = pl2;
+	    pl2 = tmp;
+	} while (merged && pl1->size() > 1);
+    }
+
+    // Set final results:
+    ite = pl1->begin();
+    nr_slice_ranges = 0;
+    int16_t offset = 0;
+    while(ite != pl1->end()) {
+	slice_ranges[nr_slice_ranges].start = ite->first;
+	slice_ranges[nr_slice_ranges].start_offset = 0;
+	slice_ranges[nr_slice_ranges].len = ite->second - ite->first;
+	slice_ranges[nr_slice_ranges].end = ite->second;
+	slice_ranges[nr_slice_ranges].slice_offset = offset;
+
+	nr_slice_ranges++;
+	offset += ite->second - ite->first;
+	++ite;
+    }
+}
+
+int16_t
+EthernetBatchProducer::get_slice_offset(const PSliceRange &psr)
+{
+    // negative is ignored
+    int16_t start = psr.start + psr.start_offset;
+
+    for (int i=0; i<nr_slice_ranges; i++) {
+	if (slice_ranges[i].start <= start &&
+	    slice_ranges[i].end >= psr.end)
+	    return slice_ranges[i].slice_offset;
+
+	// Should be, but in fact does not need to be.
+// 	if (slice_ranges[i].start <= start &&
+// 	    slice_ranges[i].end >= start + pslice_real_length(psr))
+// 	    return slice_ranges[i].slice_offset;
+    }
+
+    return -1;
+}
+
+
+PBatch::PBatch(BatchProducer *prod) : producer(prod)
+{
+}
+
+PBatch::~PBatch()
+{
+    finit();
+}
+
+int
+PBatch::init()
+{
+    npkts = 0;
+    host_mem = 0;
+    dev_mem = 0;
+    dev_stream = 0;
+    hwork_ptr = 0;
+    dwork_ptr = 0;
+    work_size = 0;
+    shared = 0;
+    priv_data = 0;
+    
+    pptrs = new Packet*[produder->batch_size];
+    if (!pptrs) {
+	click_chatter("PBatch out of mem for Packet pointers.");
+	return -1;
+    }
+    return 0;
+}
+
+void
+PBatch::finit()
+{
+    if (pptrs)
+	delete[] pptrs;
+    if (priv_data)
+	free(priv_data);
+}
+
+
+#if 0
 
 PBatch::PBatch(): capacity(CLICK_PBATCH_CAPACITY), npkts(0), pptrs(0), memsize(0),
 		  hostmem(0), devmem(0), hpktlens(0), hslices(0), hpktannos(0),
@@ -124,5 +530,7 @@ PBatch::clean_for_host_batching()
 {
     delete[] pptrs;
 }
+
+#endif
 
 CLICK_ENDDECLS
